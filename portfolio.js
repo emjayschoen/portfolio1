53ae9cfef5f5b1eb4fdbf9442962d0c8ad1f13e7
mswizzle <mschoenwustl@gmail.com>
2013-09-18 12:58:07 -0500

    Added core code for site, including header, nav bar, and portfolio content. Pre-javascript.

---
+++ portfolio.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+
+
+
+	// $.localScroll({duration:800});
+	// $("#bestDesignerEver").hover( function(event){
+	// 	$(this).hide();
+	// 	$("#bdeHover").show();
+	// });
+	
+	// $("#bdeHover").mouseout( function(event){
+	// 	$(this).hide();
+	// 	$("#bestDesignerEver").show();
+	// });
+	
+	// $(".expand").click(toggleDescription);
+	
+	// $(".contract").click(toggleDescription);
+	
+	// $(".title").click(toggleDescription);
+	
+	// function toggleDescription(){
+	// 	$(".expand").toggle();
+	// 	$(".contract").toggle();
+	// 	$(".description").slideToggle();
+	// }
+	
+	
+	// moveScroller();
+	
+	// function moveScroller() {
+	// 	var move = function() {
+	// 		var st = $(window).scrollTop();
+	// 		var ot = $("#nav-anchor").offset().top;
+	// 		var s = $("#navigationGroup");
+	// 		if(st > ot) {
+	// 			s.css({
+	// 				position: "fixed",
+	// 				top: "0px"
+	// 			});
+	// 		} else {
+	// 			if(st <= ot) {
+	// 				s.css({
+	// 					position: "relative",
+	// 					top: ""
+	// 				});
+	// 			}
+	// 		}
+	// 	};
+	// 	$(window).scroll(move);
+	// 	move();
+	// }
+	
+	
+	// var h = $(window).height(); 
+	// var w = $(window).width(); 
+	// var col1 = 530;
+	// var col2 = 400;
+	// var col3 = 60;
+	// for(var i = 1; i < 11; i++){
+	// 	$("#navTitle"+i).css("margin-left", w/2-col1-$("#navTitle"+i).width());
+	// }
+	
+	
+	// $(".nav").css("margin-left", w/2-col1);
+	// $(".highlight").css("margin-left", w/2-col1);
+	// $("#uxDesign").css("margin-left", w/2-col2);
+	// $("#bestDesignerEver").css("margin-left", w/2-col2);
+	// $("#bdeHover").css("margin-left", w/2-col2);
+	// $(".images").css("margin-left", w/2-col2);
+	// $(".divider").css("margin-left", w/2-col2);
+	// $(".title").css("margin-left", col3);
+	// $(".description").css("margin-left", col3);
+	
+	// $("#navigation1").hover( function(event){
+	// 	$("#navTitle1").toggle();
+	// });
+	// $("#navigation2").hover( function(event){
+	// 	$("#navTitle2").toggle();
+	// });
+	// $("#navigation3").hover( function(event){
+	// 	$("#navTitle3").toggle();
+	// });
+	// $("#navigation4").hover( function(event){
+	// 	$("#navTitle4").toggle();
+	// });
+	// $("#navigation5").hover( function(event){
+	// 	$("#navTitle5").toggle();
+	// });
+	// $("#navigation6").hover( function(event){
+	// 	$("#navTitle6").toggle();
+	// });
+	// $("#navigation7").hover( function(event){
+	// 	$("#navTitle7").toggle();
+	// });
+	// $("#navigation8").hover( function(event){
+	// 	$("#navTitle8").toggle();
+	// });
+	// $("#navigation9").hover( function(event){
+	// 	$("#navTitle9").toggle();
+	// });
+	// $("#navigation10").hover( function(event){
+	// 	$("#navTitle10").toggle();
+	// });
+	
+});
